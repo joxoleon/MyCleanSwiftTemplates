ef575a68aa06546e13900904a06047a5b0064c55
joxoleon <jovan.radivojsa@gmail.com>
2019-04-14 15:04:38 +0200

    Adding marker protocols and base files.

---
+++ TemplatesTestProject/TemplatesTestProject/TemplatesTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+////  ViewController.swift
+//  TemplatesTestProject
+//
+//  Created by Dejan on 4/14/19.
+//  Copyright © 2019 Jovan. All rights reserved. Dejan Borojevic
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TemplatesTestProject/TemplatesTestProject/TemplatesTestProjectTests/TemplatesTestProjectTests.swift
@@ -0,0 +1,33 @@
+////  TemplatesTestProjectTests.swift
+//  TemplatesTestProjectTests
+//
+//  Created by Dejan on 4/14/19.
+//  Copyright © 2019 Jovan. All rights reserved. Dejan Borojevic
+//
+
+import XCTest
+@testable import TemplatesTestProject
+
+class TemplatesTestProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
